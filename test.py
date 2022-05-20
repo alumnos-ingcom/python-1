47887657806b8d8a5299094cf4bc96cb6d64501e
Martín René <mrvilugron@unrn.edu.ar>
2022-05-20 12:23:48 -0300

    Agregada documentacion a las plantillas y al script que corre los tests, mas correcciones hechas por el RobotUNRN

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
-import pytest
 import sys
+import pytest
+
+"""
+Pueden utilizar este script para ejecutar los tests dentro de Thonny.
+(ejecutenló como otro programa)
+"""
 
 if __name__ == "__main__":
     sys.exit(pytest.main())
--- tests/plantilla.py
+++ tests/plantilla.py
@@ -3,6 +3,15 @@
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
+"""
+Describir aquí que es lo que se esta probando.
+Tengan en cuenta que el archivo tiene que llamarse igual
+que el archivo a probar agregando antes `test_`
+"""
 
 def test_nombrefuncion():
+    """
+       Una breve descripción del caso de prueba aplicado a la función
+    """
     pass
+
